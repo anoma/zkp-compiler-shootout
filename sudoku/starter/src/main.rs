b1d1eb355ad745039e8449e1fa23a7ac3fd39dc4
ajinkya <quaintquid@gmail.com>
2022-08-03 19:29:04 +0200

    cleanup

--- sudoku/starter/src/main.rs
+++ sudoku/starter/src/main.rs
@@ -1,10 +1,4 @@
-use std::fs;
 use starter::starter;
-use methods::{MULTIPLY_ID, MULTIPLY_PATH};
-use risc0_zkvm_core::Digest;
-use risc0_zkvm_host::Prover;
-use risc0_zkvm_serde::{from_slice, to_vec};
-use sudoku_core::Sudoku;
 
     pub fn main () {
         starter::prove_and_verify();
