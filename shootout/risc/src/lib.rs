3c5018b95bdacf4f90c4a92de5a5f2c061c28e45
mariari <mariari@protonmail.com>
2022-09-28 18:01:11 +0800

    Remove the old testing method
    We remove the functions in the folder, and replace their usage
    entirely with the new API

--- shootout/risc/src/lib.rs
+++ shootout/risc/src/lib.rs
@@ -4,9 +4,7 @@ pub use methods_fib::{FIB_ID,           FIB_PATH,
                       FIB_NINTY_TWO_ID, FIB_NINTY_TWO_PATH,
                       SUDOKU_ID,        SUDOKU_PATH};
 use risc0_zkvm_host::Prover;
-use risc0_zkvm_serde::from_slice;
 use risc0_zkvm_host::Receipt;
-use std::path::Path;
 
 #[derive(Clone)]
 pub struct Risc {
@@ -40,45 +38,11 @@ impl zero_knowledge::ZeroKnowledge for Risc {
     }
 }
 
-pub fn setup(up_to : u32) -> Prover {
-    let file_contents = fs::read(FIB_PATH).unwrap();
-    let mut prover    = Prover::new(&file_contents, FIB_ID).unwrap();
-    prover.add_input(&[up_to]).unwrap();
-    prover
-}
-
-pub fn setup_fix(method_id : &[u8], method_path : &dyn AsRef<Path>) -> Prover {
-    let file_contents = fs::read(method_path).unwrap();
-    let mut prover    = Prover::new(&file_contents, method_id).unwrap();
-    prover.add_input(&[0, 1]).unwrap();
-    prover
-}
-
-pub fn prove(prover : Prover) -> Receipt {
-    prover.run().unwrap()
-}
-
-pub fn digest(receipt : &Receipt) -> u64 {
-    let journal = receipt.get_journal_vec().unwrap();
-    from_slice(&journal).unwrap()
-}
-
-pub fn verify(method : &[u8], receipt : &Receipt) {
-    receipt.verify(method).unwrap();
-}
-
-pub fn prove_and_verify(up_to : u32) {
-    let prover  = setup(up_to);
-    let receipt = prove(prover);
-    let digest  = digest(&receipt);
-    verify(FIB_ID, &receipt);
-    println!("Ι know the secret is {}", &digest);
-}
-
-pub fn prove_and_verify_fix(method_id : &[u8], method_path : &dyn AsRef<Path>) {
-    let prover  = setup_fix(method_id, method_path);
-    let receipt = prove(prover);
-    let digest  = digest(&receipt);
-    verify(method_id, &receipt);
-    println!("Ι know the secret is {}", &digest);
+pub fn fib(input: u32) -> Risc {
+    Risc {
+        method_id: FIB_ID,
+        method_path: FIB_PATH,
+        input: vec![input],
+        name: format!("Risc0: iter-{}", input),
+    }
 }
