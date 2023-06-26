210ff1af9925b1c3374e18ba43a90677f6403a53
mariari <mariari@protonmail.com>
2023-06-26 13:29:15 +0800

    Update main properly
    The code would not compile before and would cause an issue in building

--- shootout/vampir-halo2/src/main.rs
+++ shootout/vampir-halo2/src/main.rs
@@ -1,8 +1,8 @@
-use vampir_plonk;
+use vampir_halo2;
 use zero_knowledge::ZeroKnowledge;
 
 pub fn main() {
-    let circuit = vampir_plonk::VampIRCircuit {
+    let circuit = vampir_halo2::VampIRCircuit {
         path: "sudoku.pir",
         name: "Vamp-IR halo2: Sudoku".to_string(),
     };
